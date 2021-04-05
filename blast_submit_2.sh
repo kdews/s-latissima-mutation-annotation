0f6f4f0ffef77e49b710e96fa1c47baf40494b52
kellywithsword <kdeweese@usc.edu>
2021-04-05 14:40:51 -0700

    Added scripts used to annotate contaminated reads in WGS data

---
+++ blast_submit_2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH --mem=20gb
+#SBATCH --time=08:00:00
+#SBATCH --cpus-per-task=12
+
+blastn -num_threads 12 -query blast/${1}_R2.bad.fa -db nt -perc_identity 95 -out blast/${1}_R2.blast.out -outfmt "6 qseqid sseqid pident length"
+

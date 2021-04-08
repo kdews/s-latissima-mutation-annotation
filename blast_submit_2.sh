0212214405afba662810ea9f912524f10c631eca
kellywithsword <kdeweese@usc.edu>
2021-04-07 17:36:25 -0700

    Changed BLAST time allocations

--- blast_submit_2.sh
+++ blast_submit_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --mem=20gb
-#SBATCH --time=20:00:00
-#SBATCH --cpus-per-task=32
+#SBATCH --time=120:00:00
+#SBATCH --cpus-per-task=12
 
-blastn -num_threads 32 -query blast/${1}_R2.bad.fa -db nt -perc_identity 95 -out blast/${1}_R2.blast.out -outfmt "6 qseqid sseqid pident length"
+blastn -num_threads 12 -query blast/${1}_R2.bad.fa -db nt -perc_identity 95 -out blast/${1}_R2.blast.out -outfmt "6 qseqid sseqid pident length"
 
